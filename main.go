d0334f71e99597206a2f6042b3344291d09df032
Pavel <pavel.oliynyk@gmail.com>
2025-05-19 12:34:42 +0100

    Play with json encoding/decoding

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
-
 type User struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
@@ -17,23 +11,23 @@ type MyData struct {
 	data string `json:"data"`
 }
 
-func main() {
-	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
-		var data MyData = MyData{Id: "test", data: "Go"}
-		json.NewDecoder(r.Body).Decode(&data)
-
-		fmt.Fprintf(w, "This is our data: %s %s", data.Id, data.data)
-	})
-
-	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
-		peter := User{
-			Firstname: "John",
-			Lastname:  "Doe",
-			Age:       25,
-		}
-
-		json.NewEncoder(w).Encode(peter)
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
+//func main() {
+//	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+//		var data MyData = MyData{Id: "test", data: "Go"}
+//		json.NewDecoder(r.Body).Decode(&data)
+//
+//		fmt.Fprintf(w, "This is our data: %s %s", data.Id, data.data)
+//	})
+//
+//	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+//		peter := User{
+//			Firstname: "John",
+//			Lastname:  "Doe",
+//			Age:       25,
+//		}
+//
+//		json.NewEncoder(w).Encode(peter)
+//	})
+//
+//	http.ListenAndServe(":8080", nil)
+//}
